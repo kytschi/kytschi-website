6dd9e697cf844acb0106d3f487a6894bc9af36f5
Mike Welsh <dev@kytschi.com>
2023-08-29 13:12:56 +0100

    tweaks to pull in data from future site

--- past/public/index.php
+++ past/public/index.php
@@ -1,10 +1,3 @@
 <?php
 
-use KytschBASIC\Compiler;
-use KytschBASIC\Exceptions\Exception as KytschException;
-
-try {
-    (new Compiler(__DIR__ . '/../config'))->run();
-} catch (\Exception $err) {
-    (new KytschException($err->getMessage()))->fatal();
-}
+(new KytschBASIC\Compiler(__DIR__ . '/../config'))->run();
